c85fb92a37ef25335cf28f71df32a3c0a65bb94a
ckw017 <christopherwong888@gmail.com>
2016-07-24 18:38:17 -0700

    Added Tools class
    Currently used to grab and convert images from resources into
    BufferedImage

---
+++ src/Asteroids/Tools.java
@@ -0,0 +1,14 @@
+package Asteroids;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Tools{
+    public static BufferedImage getImage(String path){
+        BufferedImage rImage;
+        try{rImage = ImageIO.read(Tools.class.getResource("/resources/" + path));}
+        catch(IOException ex){rImage = null;}
+        return rImage;
+    }
+}
