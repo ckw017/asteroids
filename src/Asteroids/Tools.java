30be65bdaa4bf7ca17ed1ebb53a8cee1b353a51e
ckw017 <christopherwong888@gmail.com>
2016-08-11 22:06:30 -0700

    Adjusted image retrieval method

--- src/Asteroids/Tools.java
+++ src/Asteroids/Tools.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Tools{
     public static BufferedImage getImage(String path){
         BufferedImage rImage;
-        try{rImage = ImageIO.read(Tools.class.getResource("/resources/" + path));}
+        try{rImage = ImageIO.read(Tools.class.getResource("/" + path));}
         catch(IOException ex){rImage = null;}
         return rImage;
     }
